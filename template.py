995e503efd07fe7c2b77ef733fc279868cebe7ee
ParthLathiya <lathiyap46@gmail.com>
2024-01-21 19:46:25 +0530

    folder structures added

--- template.py
+++ template.py
@@ -1,1 +1,54 @@
 import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO,format="[%(asctime)s] : %(message)s")
+
+project_name ="mlProjects"
+
+list_of_files = [
+
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utills/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configurations.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",  
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constant/__init__.py",       
+
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "app.py",
+    "requirements.txt",
+    "setups.py",
+    "reserch/trials.igpynb",
+    "templates/index.html"
+
+
+
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+
+    filedir,filename = os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir,exist_ok = True)
+        logging.info(f"Creating Directory; {filedir} for the file: {filename}")
+
+    if(not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,"w") as f:
+            pass
+        logging.info(f"crearing emty file {filepath}")
+
+
+    else:
+        logging.info(f"{filename} is already exits")
+
